1f0ea0974a125ed804d2e33df36d000bb5441024
ChetRosie <169575341+chetrosie@users.noreply.github.com>
2026-03-11 16:37:50 +0800

    feat: active teek theme

--- .vitepress/theme/index.mjs
+++ .vitepress/theme/index.mjs
@@ -1,10 +1,6 @@
 import TeekTheme from 'vitepress-theme-teek'
-// 修正后的 CSS 引入路径
 import 'vitepress-theme-teek/bundle.css'
 
 export default {
-  ...TeekTheme,
-  enhanceApp({ app, router, siteData }) {
-    // 保持为空即可
-  }
+  ...TeekTheme
 }
